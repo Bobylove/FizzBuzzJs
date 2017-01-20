85d39f521cda77d38e51485c8b218b095e93f01e
Fred <delon.frederic.pro.job@gmail.com>
2017-01-20 14:31:02 +0100

    Etape 1: affiché fizz pour les entiers divisible par 3

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -1,9 +1,12 @@
-var FizzBuzzOf = require('../lib/FizzBuzz').FizzBuzzOf
+var FizzBuzzOf = require('../lib/izzBuzz').FizzBuzzOf
 
 describe("FizzBuzzOf execution", function () {
 
     it("should return ChangeMe at the beginning with parameter 1", function () {
         expect(FizzBuzzOf(1)).toBe("ChangeMe");
     });
+    it("should return Fizz at the beginning with parameter 3 %", function(){
+    	expect(FizzBuzzOf(%3)).toBe("Fizz");
+    })
 
 });
