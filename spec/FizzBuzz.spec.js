511343ae6df8a71a8e9f018582f3cc7f19ffa151
Fred <delon.frederic.pro.job@gmail.com>
2017-01-20 14:49:15 +0100

    step 2 clear modulo 5 to buzz

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -1,4 +1,4 @@
-var FizzBuzzOf = require('../lib/izzBuzz').FizzBuzzOf
+var FizzBuzzOf = require('../lib/fizzBuzz').FizzBuzzOf
 
 describe("FizzBuzzOf execution", function () {
 
@@ -6,7 +6,11 @@ describe("FizzBuzzOf execution", function () {
         expect(FizzBuzzOf(1)).toBe("ChangeMe");
     });
     it("should return Fizz at the beginning with parameter 3 %", function(){
-    	expect(FizzBuzzOf(%3)).toBe("Fizz");
-    })
+    	expect(FizzBuzzOf(3)).toBe("fizz");
+    });
+
+     it("should return buzz at the beginning with parameter 5 %", function(){
+    	expect(FizzBuzzOf(5)).toBe("buzz");
+    });
 
 });
